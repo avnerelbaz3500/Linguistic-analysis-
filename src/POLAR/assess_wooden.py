13f4d55502d03b7a5b32918664c7948f4fb74f72
Votre Nom <gabriel.francois@ensae.fr>
2026-04-16 16:38:28 +0200

    Add POLAR + plots

--- src/POLAR/assess_wooden.py
+++ src/POLAR/assess_wooden.py
@@ -6,20 +6,13 @@ from pathlib import Path
 import pandas as pd
 import numpy as np
 import torch
-import nltk
+import spacy
 import matplotlib.pyplot as plt
 import seaborn as sns
 from transformers import AutoTokenizer, AutoModel
 from tqdm import tqdm
 
 from helper_function.print import *
-
-def setup_nltk():
-    """Downloads required NLTK data for sentence tokenization."""
-    try:
-        nltk.data.find('tokenizers/punkt')
-    except LookupError:
-        nltk.download('punkt', quiet=True)
 
 def get_device() -> torch.device:
     """Returns the optimal device (MPS for Apple Silicon, CUDA for Nvidia, else CPU)."""
@@ -73,8 +66,8 @@ def build_polar_axis(pairs_path: Path, tokenizer, model, device) -> torch.Tensor
         raise ValueError(red("Invalid pairs dataset format or empty dataset."))
         
     print("Embedding base dataset to construct POLAR axis...")
-    embed_direct = embed_sentences(direct_texts, tokenizer, model, device)
-    embed_ldb = embed_sentences(ldb_texts, tokenizer, model, device) 
+    embed_direct = embed_sentences(direct_texts, tokenizer, model, device) # (num_sentences, hidden_size)
+    embed_ldb = embed_sentences(ldb_texts, tokenizer, model, device) # (num_sentences, hidden_size)
     
     polar_vector = torch.mean(embed_ldb - embed_direct, dim=0) # mean over the number of sentences
     polar_vector = torch.nn.functional.normalize(polar_vector, p=2, dim=0)
@@ -93,12 +86,12 @@ def compute_polar_scores_bulk(sentences: list, tokenizer, model, device, polar_v
         ).to(device)
         
         with torch.no_grad():
-            model_output = model(**encoded_input)
+            model_output = model(**encoded_input) # model_output[0] : (batch_size, seq_length, hidden_size)
             
-        embeddings = mean_pooling(model_output, encoded_input['attention_mask'])
+        embeddings = mean_pooling(model_output, encoded_input['attention_mask']) # (batch_size, hidden_size)
         embeddings = torch.nn.functional.normalize(embeddings)
         
-        scores = torch.matmul(embeddings, polar_vector)
+        scores = torch.matmul(embeddings, polar_vector) # (batch_size, hidden_size) @ (hidden_size) -> (batch_size)
         all_scores.extend(scores.cpu().numpy())
         
     return np.array(all_scores)
@@ -108,13 +101,11 @@ def main():
     parser.add_argument("--pairs", type=str, default="src/POLAR/data/pairs.json", help="Path to the generated pairs.json file.")
     parser.add_argument("--data", type=str, default="data/clean/archelect_clean.parquet", help="Path to the archelect_clean parquet file.")
     parser.add_argument("--model", type=str, default="dangvantuan/sentence-camembert-base", help="HuggingFace model for embeddings.")
-    parser.add_argument("--output-graph", type=str, default="logs/POLAR/ldb_evolution.png", help="Path to save the output graph.")
+    parser.add_argument("--output-data", type=str, default="data/POLAR/", help="Path to save the output graph.")
     parser.add_argument("--batch-size", type=int, default=32, help="Batch size for embedding generation.")
     args = parser.parse_args()
     
-    os.makedirs(args.output_graph, exist_ok=True)
-    setup_nltk()
-    nltk.download('punkt_tab')
+    os.makedirs(args.output_data, exist_ok=True)
     device = get_device()
     print(bold(f"Using device: {device}"))
     
@@ -134,12 +125,16 @@ def main():
     df['year'] = pd.to_datetime(df['date']).dt.year
     
     print(blue("Tokenizing and flattening corpus..."))
+    nlp = spacy.blank("fr")
+    nlp.add_pipe("sentencizer")
     doc_indices = []
     all_sentences = []
     
-    for idx, text in enumerate(df['raw_text']):
-        if isinstance(text, str) and text.strip():
-            sentences = nltk.sent_tokenize(text, language='french')
+    texts = df['raw_text'].fillna("").tolist()
+    
+    for idx, doc in tqdm(enumerate(nlp.pipe(texts, batch_size=300)), total=len(texts), desc="Tokenisation"):
+        if doc.text.strip():
+            sentences = [sent.text.strip() for sent in doc.sents if sent.text.strip()]
             all_sentences.extend(sentences)
             doc_indices.extend([idx] * len(sentences)) # Store indices 
     print(blue(f"Total sentences to process globally: {len(all_sentences)}"))
@@ -154,27 +149,9 @@ def main():
     df.loc[mean_scores.index, 'ldb_score'] = mean_scores.values
     df = df.dropna(subset=['ldb_score'])
     
-    print(blue("Generating visualization..."))
-    plt.figure(figsize=(14, 8))
-    sns.set_theme(style="whitegrid")
+    output_df_path = os.path.join(args.output_data, "archelect_scored.parquet")
+    print(blue(f"Saving new data_base with POLAR in : {output_df_path}"))
+    df.to_parquet(output_df_path, index=False)
     
-    sns.lineplot(
-        data=df, 
-        x='year', 
-        y='ldb_score', 
-        hue='affiliate political party', 
-        marker='o',
-        errorbar=None 
-    )
-    
-    plt.title("Evolution of 'Langue de Bois' in French Political Speeches", fontsize=16, pad=20)
-    plt.ylabel("POLAR Projection Score (Langue de Bois)", fontsize=12)
-    plt.xlabel("Year", fontsize=12)
-    plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left', title="Political Party")
-    plt.tight_layout()
-    
-    plt.savefig(args.output_graph, dpi=300)
-    print(green(f"Analysis complete. Graph saved to {args.output_graph}"))
-
 if __name__ == "__main__":
     main()
---
+++ src/POLAR/generate_plot.py
@@ -0,0 +1,121 @@
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import argparse
+
+from helper_function.print import *
+
+def main():
+    parser = argparse.ArgumentParser(description="POLAR projection for political rhetoric analysis.")
+    parser.add_argument("--output-graph", type=str, default="logs/POLAR/", help="Path to save the output graph.")
+    parser.add_argument("--data-path", type=str, default="data/POLAR/archelect_scored.parquet", help="Path to save the output graph.")
+    args = parser.parse_args()
+    
+    plt.style.use('ggplot')
+    sns.set_palette("viridis")
+    os.makedirs(args.output_graph, exist_ok=True)
+    
+    df = pd.read_parquet(args.data_path)
+    
+    print(blue("Generating visualization..."))
+    plt.figure(figsize=(14, 8))
+    sns.set_theme(style="whitegrid")
+    
+    sns.lineplot(
+        data=df, 
+        x='year', 
+        y='ldb_score', 
+        hue='affiliate political party', 
+        marker='o',
+        errorbar=None 
+    )
+    
+    plt.title("Evolution of 'Langue de Bois' in French Political Speeches\n(Score toward +1 = Wooden Language | Score toward -1 = Direct Speech)", fontsize=16, pad=20)
+    plt.ylabel("POLAR Projection Score", fontsize=12)
+    plt.xlabel("Year", fontsize=12)
+    plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left', title="Political Party")
+    plt.tight_layout()
+    
+    plt.savefig(os.path.join(args.output_graph, "polar_evolution.png"), dpi=300)
+    
+    print(blue("Boxplot generation..."))
+    plt.figure(figsize=(12, 8))
+    order = df.groupby('affiliate political party')['ldb_score'].median().sort_values(ascending=False).index
+    
+    sns.boxplot(
+        data=df, 
+        y='affiliate political party', 
+        x='ldb_score', 
+        order=order,
+        palette="magma", 
+        hue='affiliate political party'
+    )
+    plt.title("Repartition of 'Langue de Bois' by political party (1981-1993)\n(Score toward +1 = Wooden Language | Score toward -1 = Direct Speech)", fontsize=16)
+    plt.xlabel("Wooden language score (POLAR)", fontsize=12)
+    plt.ylabel("Political party", fontsize=12)
+    plt.axvline(x=0, color='red', linestyle='--', alpha=0.5) 
+    plt.tight_layout()
+    plt.savefig(os.path.join(args.output_graph, "ldb_boxplot_partis.png"), dpi=300)
+
+    print(blue("Generate density curve..."))
+    plt.figure(figsize=(10, 6))
+    sns.kdeplot(data=df, x='ldb_score', fill=True, color="dodgerblue", alpha=0.5)
+    plt.title("Overall distribution of wooden language scores", fontsize=16)
+    plt.xlabel("POLAR score", fontsize=12)
+    plt.ylabel("Density", fontsize=12)
+    plt.axvline(x=0, color='red', linestyle='--', alpha=0.5)
+    plt.tight_layout()
+    plt.savefig(os.path.join(args.output_graph, "ldb_distribution.png"), dpi=300)
+    
+    print(blue("By sex"))
+    plt.figure(figsize=(12, 6))
+    sns.barplot(data=df, x='ldb_score', y='titulaire-sexe', palette="viridis", hue="titulaire-sexe", errorbar=None)
+    plt.title("'langue de bois' according to the gender \n(Highest scores closest to +1)", fontsize=16)
+    plt.xlabel("POLAR score", fontsize=12)
+    plt.ylabel("sex", fontsize=12)
+    plt.tight_layout()
+    plt.savefig(os.path.join(args.output_graph, "ldb_sex.png"), dpi=300)
+    
+    print(blue("By age"))
+    df['titulaire-age'] = pd.to_numeric(df['titulaire-age'], errors='coerce')
+    bins = [18, 30, 40, 50, 60, 70, 120]
+    labels = ['18-29 ans', '30-39 ans', '40-49 ans', '50-59 ans', '60-69 ans', '70 ans et +']
+    df['age_group'] = pd.cut(df['titulaire-age'], bins=bins, labels=labels, right=False)
+    plt.figure(figsize=(12, 6))
+    sns.barplot(
+        data=df, 
+        x='ldb_score', 
+        y='age_group', 
+        palette="cubehelix", 
+        hue="age_group",
+        order=labels, 
+        legend=False,
+        errorbar=None
+    )
+    plt.title("'Langue de bois' according to age group\n(Highest scores closest to +1)", fontsize=16)
+    plt.xlabel("POLAR score", fontsize=12)
+    plt.ylabel("Age Group", fontsize=12) 
+    plt.tight_layout()
+    plt.savefig(os.path.join(args.output_graph, "ldb_age.png"), dpi=300)
+    
+    print(blue("Generating visualization by titulaire-name..."))
+    if 'titulaire-nom' in df.columns and 'titulaire-prenom' in df.columns: 
+        print(blue("Top 10..."))
+        df['name'] = df['titulaire-prenom'] + " " + df['titulaire-nom']
+        top_10 = df.nlargest(10, 'ldb_score')
+        
+        plt.figure(figsize=(12, 6))
+        sns.barplot(data=top_10, x='ldb_score', y='name', palette="Reds_r", hue="name")
+        plt.title("Top 10 most wooden language speeches\n(Highest scores closest to +1)", fontsize=16)
+        plt.xlabel("POLAR score", fontsize=12)
+        plt.ylabel("Candidate", fontsize=12)
+        plt.tight_layout()
+        plt.savefig(os.path.join(args.output_graph, "ldb_top10.png"), dpi=300)
+    else:
+        print(red("Warning: Missing columns 'titulaire-nom' or 'titulaire-prenom', ignoring top ten graph."))
+        
+    print(green(f"Analysis complete. Graphs saved to {args.output_graph}"))
+
+if __name__ == "__main__":
+    main()
